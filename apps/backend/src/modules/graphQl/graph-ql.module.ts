1439dcc46f895b42ddcbddd3884dfbdb4eb0d93f
mahmoud-farag <mahmoud32salamn@gmail.com>
2026-04-05 10:27:05 +0200

    register the graphql as a module

--- apps/backend/src/modules/graphQl/graph-ql.module.ts
+++ apps/backend/src/modules/graphQl/graph-ql.module.ts
@@ -2,13 +2,24 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'node:path';
+import { CompanyModule } from '../company/company.module';
+import { CompanyRegistryService } from '../company/company-client-registry.service';
+import type { Request } from 'express';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/gql/schema.gql'),
-      graphiql: true,
+      imports: [CompanyModule],
+      inject: [CompanyRegistryService],
+      useFactory: (companyRegistry: CompanyRegistryService) => ({
+        autoSchemaFile: join(process.cwd(), 'src/gql/schema.gql'),
+        graphiql: true,
+        context: async ({ req }: { req: Request }) => ({
+          req,
+          db: await companyRegistry.getCompanyDb(req.hostname),
+        }),
+      }),
     }),
   ],
   providers: [],
